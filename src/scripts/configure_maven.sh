a9fe60f01ee6b87a0ffec00c65a9e780e80e02a2
Seraphina Anderson <seraphina.anderson@gmail.com>
2024-05-09 16:00:37 +0100

    Create scripts

--- src/scripts/configure_maven.sh
+++ src/scripts/configure_maven.sh
@@ -5,18 +5,12 @@
 
 set +e
 
-if [ -z "$CLOUDSMITH_NPM_REGISTRY" ]
+if [ -z "$CLOUDSMITH_MAVEN_REGISTRY" ]
 then
-  echo "Unable to configure NPM. Env var CLOUDSMITH_NPM_REGISTRY is not defined. Please run the set_env_vars_for_npm command first."
-  exit 1
-fi
-if [ -z "$CLOUDSMITH_NPM_AUTH_CONFIG" ]
-then
-  echo "Unable to configure NPM. Env var CLOUDSMITH_NPM_AUTH_CONFIG is not defined. Please run the set_env_vars_for_npm command first."
+  echo "Unable to configure MAVEN. Env var CLOUDSMITH_MAVEN_REGISTRY is not defined. Please run the set_env_vars_for_maven command first."
   exit 1
 fi
 
-npm config set registry="$CLOUDSMITH_NPM_REGISTRY"
-npm config set "$CLOUDSMITH_NPM_AUTH_CONFIG"
+maven config set registry="$CLOUDSMITH_MAVEN_REGISTRY"
 
-echo "NPM has been configured to use Cloudsmith registry with $CLOUDSMITH_NPM_REGISTRY."
+echo "MAVEN has been configured to use Cloudsmith registry with $CLOUDSMITH_MAVEN_REGISTRY."
---
+++ src/scripts/generate_settings_file.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir ~/.m2 && touch ~/.m2/settings.xml
+echo -e "<settings>\n\t<servers>\n\t\t<server>\n\t\t\t<id>financial-times-financial-times-internal-releases</id>\n\t\t\t<username>$CLOUDSMITH_SERVICE_ACCOUNT</username>\n\t\t\t<password>$CLOUDSMITH_API_KEY</password>\n\t\t</server>\n\t</servers>\n</settings>" > ~/.m2/settings.xml
