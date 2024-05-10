e0a16dece94db2b2756e83ec7a67b2d5bf71ffaf
Seraphina Anderson <seraphina.anderson@gmail.com>
2024-05-10 15:35:03 +0100

    Remove .m2 folder creation

--- src/scripts/generate_settings_file.sh
+++ src/scripts/generate_settings_file.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-mkdir ~/.m2 && touch ~/.m2/settings.xml
+# mkdir ~/.m2 && touch ~/.m2/settings.xml
+touch ~/.m2/settings.xml
 echo -e "<settings>\n\t<servers>\n\t\t<server>\n\t\t\t<id>financial-times-financial-times-internal-releases</id>\n\t\t\t<username>$CLOUDSMITH_SERVICE_ACCOUNT</username>\n\t\t\t<password>$CLOUDSMITH_OIDC_KEY</password>\n\t\t</server>\n\t</servers>\n</settings>" > ~/.m2/settings.xml
