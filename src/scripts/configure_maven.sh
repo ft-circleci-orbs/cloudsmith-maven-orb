ff4be5d91aac57080752834dabfcdec17e47ca53
Seraphina Anderson <seraphina.anderson@gmail.com>
2024-05-10 14:53:47 +0100

    Add a couple more param tests and remove redundant code

--- src/scripts/configure_maven.sh
+++ src/scripts/configure_maven.sh
@@ -11,6 +11,17 @@ then
   exit 1
 fi
 
-maven config set registry="$CLOUDSMITH_MAVEN_REGISTRY"
+if [ -z "$CLOUDSMITH_REPOSITORY" ]
+then
+  echo "Unable to configure MAVEN. Env var CLOUDSMITH_REPOSITORY is not defined. Please run the set_env_vars_for_maven command first."
+  exit 1
+fi
+
+if [ -z "$CLOUDSMITH_OIDC_TOKEN" ]
+then
+  echo "Unable to configure MAVEN. Env var CLOUDSMITH_OIDC_TOKEN is not defined. Please run the set_env_vars_for_maven command first."
+  exit 1
+fi
+
 
 echo "MAVEN has been configured to use Cloudsmith registry with $CLOUDSMITH_MAVEN_REGISTRY."
