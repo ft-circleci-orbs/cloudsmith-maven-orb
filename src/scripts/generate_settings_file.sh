42dc5028a89471b91f7a99a5859fd1b5713e0670
Seraphina Anderson <seraphina.anderson@gmail.com>
2024-05-10 16:32:52 +0100

    Replace parameter

--- src/scripts/generate_settings_file.sh
+++ src/scripts/generate_settings_file.sh
@@ -2,5 +2,5 @@
 
 # Create .m2 directory and settings.xml file
 mkdir ~/.m2 && touch ~/.m2/settings.xml
-echo -e "<settings>\n\t<servers>\n\t\t<server>\n\t\t\t<id>financial-times-$CLOUDSMITH_REPOSITORY</id>\n\t\t\t<username>$CLOUDSMITH_SERVICE_ACCOUNT</username>\n\t\t\t<password>$CLOUDSMITH_OIDC_KEY</password>\n\t\t</server>\n\t</servers>\n</settings>" > ~/.m2/settings.xml
+echo -e "<settings>\n\t<servers>\n\t\t<server>\n\t\t\t<id>financial-times-$CLOUDSMITH_REPOSITORY</id>\n\t\t\t<username>$CLOUDSMITH_SERVICE_ACCOUNT</username>\n\t\t\t<password>$CLOUDSMITH_MAVEN_TOKEN</password>\n\t\t</server>\n\t</servers>\n</settings>" > ~/.m2/settings.xml
 cat ~/.m2/settings.xml
