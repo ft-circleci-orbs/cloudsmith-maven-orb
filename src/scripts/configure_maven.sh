c336821d6d0ad50d9535ff3b5402a0708d83a00d
Seraphina Anderson <seraphina.anderson@gmail.com>
2024-06-05 11:44:47 +0100

    Tweak logic

--- src/scripts/configure_maven.sh
+++ src/scripts/configure_maven.sh
@@ -18,37 +18,33 @@ then
   exit 1
 fi
 
-echo "MAVEN has been configured to use Cloudsmith repository with $CLOUDSMITH_MAVEN_REPOSITORY_URL."
-
-
-# Check for existence of .m2 & settings.xml and create them if they don't exist
+# Check for existence of .m2 and create it if it doesn't exist
 if [ -d "${HOME}/.m2" ]
 then
   echo "${HOME}/.m2 exists"
-  if [ -f "${HOME}/.m2/settings.xml" ]
-  then
-    echo "settings.xml exists"
-  else
-    echo "settings.xml does not exist"
-    touch settings.xml
-  fi
 else
-  echo "${HOME}/.m2 does not exist. Creating ${HOME}/.m2/settings.xml"
+  echo "${HOME}/.m2 does not exist. Creating ${HOME}/.m2"
   mkdir "${HOME}/.m2"
-  touch "${HOME}/.m2/settings.xml"
+fi
+
+if [ -f "${HOME}/.m2/settings.xml" ]
+then
+  echo "${HOME}/.m2/settings.xml already exists. Unable to configure for Cloudsmith."
+else
+  echo "${HOME}/.m2/settings.xml does not exist. Creating..."
   cat >> "${HOME}/.m2/settings.xml"<< EOF
-  <settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
-          xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-          xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0
-                          https://maven.apache.org/xsd/settings-1.0.0.xsd">
-    <servers>
-        <server>
-            <id>\${env.CLOUDSMITH_REPO_ID}</id>
-            <username>\${env.CLOUDSMITH_SERVICE_ACCOUNT}</username>
-            <password>\${env.CLOUDSMITH_MAVEN_TOKEN}</password>
-        </server>
-    </servers>
-  </settings>
+<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0
+                        https://maven.apache.org/xsd/settings-1.0.0.xsd">
+  <servers>
+      <server>
+          <id>\${env.CLOUDSMITH_REPO_ID}</id>
+          <username>\${env.CLOUDSMITH_SERVICE_ACCOUNT}</username>
+          <password>\${env.CLOUDSMITH_MAVEN_TOKEN}</password>
+      </server>
+  </servers>
+</settings>
 EOF
 fi
 
