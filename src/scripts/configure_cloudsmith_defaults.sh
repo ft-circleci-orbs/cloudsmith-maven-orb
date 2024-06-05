a750afe750e523a06fa0b6b65d32bb0d654a20b4
Seraphina Anderson <seraphina.anderson@gmail.com>
2024-06-05 11:22:40 +0100

    remove default repo

--- src/scripts/configure_cloudsmith_defaults.sh
+++ src/scripts/configure_cloudsmith_defaults.sh
@@ -10,9 +10,4 @@ then
   echo "export CLOUDSMITH_DOWNLOADS_DOMAIN=\"packages.ft.com\"" >> "$BASH_ENV"
 fi
 
-if [ -z "$CLOUDSMITH_REPOSITORY" ]
-then
-  echo "export CLOUDSMITH_REPOSITORY=\"financial-times-internal-releases\"" >> "$BASH_ENV"
-fi
-
 echo "export CLOUDSMITH_MAVEN_UPLOAD_DOMAIN=\"maven.packages.ft.com\"" >> "$BASH_ENV"
